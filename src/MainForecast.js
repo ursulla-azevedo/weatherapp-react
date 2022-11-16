be35920daa5309a53e9348602a47b7a8762e0529
ursulla-azevedo <ursullazevedo@gmail.com>
2022-11-16 14:29:49 +0000

    Updated weather images for main forecast

--- src/MainForecast.js
+++ src/MainForecast.js
@@ -21,7 +21,7 @@ export default function MainForecast(props) {
       wind: Math.round(response.data.wind.speed),
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
     });
   }
 
--- src/MainWeatherInfo.js
+++ src/MainWeatherInfo.js
@@ -4,9 +4,60 @@ import AppDate from "./AppDate";
 
 import lowesttemp from "./media/weather-elements_Temperature-low.svg";
 import highesttemp from "./media/weather-elements_Temperature-high.svg";
-import currentweatherimg from "./media/weather-elements_Sun-clouds.svg";
+
+import weatherSun from "./media/weather-elements_Sun.svg";
+import weatherMoon from "./media/weather-elements_Moon.svg";
+import weatherCloudsD from "./media/weather-elements_Sun-clouds.svg";
+import weatherCloudsN from "./media/weather-elements_Moon-clouds.svg";
+import weatherClouds from "./media/weather-elements_Clouds.svg";
+import weatherCloudsMore from "./media/weather-elements_Clouds-more.svg";
+import weatherRain from "./media/weather-elements_Rain.svg";
+import weatherStorm from "./media/weather-elements_Storm.svg";
+import weatherSnow from "./media/weather-elements_Snow.svg";
+import weatherWind from "./media/weather-elements_Wind.svg";
 
 export default function MainWeatherInfo(props) {
+  let weatherImg = props.data.icon;
+  let weatherTodayImgSrc = "";
+  let weatherTodayImgAlt = "";
+
+  if (weatherImg === "01d") {
+    weatherTodayImgSrc = weatherSun;
+    weatherTodayImgAlt = "Sun";
+  } else if (weatherImg === "01n") {
+    weatherTodayImgSrc = weatherMoon;
+    weatherTodayImgAlt = "Moon";
+  } else if (weatherImg === "02d") {
+    weatherTodayImgSrc = weatherCloudsD;
+    weatherTodayImgAlt = "Sun and clouds";
+  } else if (weatherImg === "02n") {
+    weatherTodayImgSrc = weatherCloudsN;
+    weatherTodayImgAlt = "Moon and clouds";
+  } else if (weatherImg === "03d" || weatherImg === "03n") {
+    weatherTodayImgSrc = weatherClouds;
+    weatherTodayImgAlt = "Clouds";
+  } else if (weatherImg === "04d" || weatherImg === "04n") {
+    weatherTodayImgSrc = weatherCloudsMore;
+    weatherTodayImgAlt = "Heavy clouds";
+  } else if (
+    weatherImg === "09d" ||
+    weatherImg === "09n" ||
+    weatherImg === "10d" ||
+    weatherImg === "10n"
+  ) {
+    weatherTodayImgSrc = weatherRain;
+    weatherTodayImgAlt = "Rain";
+  } else if (weatherImg === "11d" || weatherImg === "11n") {
+    weatherTodayImgSrc = weatherStorm;
+    weatherTodayImgAlt = "Storm";
+  } else if (weatherImg === "13d" || weatherImg === "13n") {
+    weatherTodayImgSrc = weatherSnow;
+    weatherTodayImgAlt = "Snow";
+  } else if (weatherImg === "50d" || weatherImg === "50n") {
+    weatherTodayImgSrc = weatherWind;
+    weatherTodayImgAlt = "Wind";
+  }
+
   return (
     <div>
       <h2>
@@ -61,8 +112,8 @@ export default function MainWeatherInfo(props) {
           </div>
           <div className="col-6 current-weather-img-block">
             <img
-              src={currentweatherimg}
-              alt="Sun and clouds"
+              src={weatherTodayImgSrc}
+              alt={weatherTodayImgAlt}
               className="current-weather-image"
             />
           </div>
