04a61a1e1c08dc46717bc34a477e1cd4dd0b8843
ursulla-azevedo <ursullazevedo@gmail.com>
2022-11-17 14:16:02 +0000

    Fixed forecast update bug

--- src/ExtraForecast.js
+++ src/ExtraForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./styles/extraforecast.css";
 
@@ -7,6 +7,10 @@ import ForecastEach from "./ForecastEach";
 export default function ExtraForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data);
